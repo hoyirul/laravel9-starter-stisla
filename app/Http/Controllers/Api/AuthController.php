977d9a36863c7501b1ab05baaaf5ea18fc218ac2
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-12-17 00:39:53 +0700

    Fix Login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,35 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     use ApiResponse;
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-            // 'device_name' => 'required',
-        ]);
-
-        $user = User::where('email', $request->email)->first();
-        
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+        $validated = $request->validated();
+        if(!Auth::attempt($validated)){
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
         }
+        $user = User::where('email', $validated['email'])->first();
 
         // $user->assignRole('user');
         return response()->json(
